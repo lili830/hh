bc12eb4c96ba4e897ee320775a74249875da22d4
vivi <vivi@LAPTOP-QQEUEPVJ>
2020-07-22 22:18:08 +0800

    修改1

--- src/hhbd/HelloHH.java
+++ src/hhbd/HelloHH.java
@@ -5,6 +5,7 @@ public class HelloHH {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloHH!!");
+		System.out.println("修改update");
 	}
 
 }
